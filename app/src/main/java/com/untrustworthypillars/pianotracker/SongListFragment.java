f5ee52e015495105fd005a65d77a948177ab9b33
Karolis J <jasinevicius4@gmail.com>
2019-08-04 13:23:26 +0300

    Added SubMenus and onClickers for SongListFragment

--- app/src/main/java/com/untrustworthypillars/pianotracker/SongListFragment.java
+++ app/src/main/java/com/untrustworthypillars/pianotracker/SongListFragment.java
@@ -3,13 +3,14 @@ package com.untrustworthypillars.pianotracker;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.res.ResourcesCompat;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -36,6 +37,13 @@ public class SongListFragment extends Fragment {
     private RecyclerView mSongRecyclerView;
     private SongAdapter mAdapter;
     private Callbacks mCallbacks;
+    private boolean mFilterStateIsActive = false;
+    private int mFilterState = 0;
+    private boolean mFilterDifficultyIsActive = false;
+    private int mFilterDifficulty = 0;
+    private boolean mSortIsActive = false;
+    private int mSort = 0;
+
 
     // Required interface for hosting activities
     public interface Callbacks {
@@ -99,7 +107,7 @@ public class SongListFragment extends Fragment {
         @Override
         public void onClick(View view) {
             mCallbacks.onSongSelected(mSong);
-        }
+        } //TODO once filtering is done, need to pass the whole list or maybe parameters to query that specific list
 
     }
 
@@ -154,6 +162,9 @@ public class SongListFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.list_toolbar, menu);
         MenuItem filterItem = menu.findItem(R.id.list_toolbar_filter);
+//        Drawable filterIcon = filterItem.getIcon();
+//        filterIcon.setTint(getResources().getColor(R.color.difficulty_very_hard, getContext().getTheme()));
+        MenuItem filterStateItem = menu.findItem(R.id.list_toolbar_filter_state);
         MenuItem sortItem = menu.findItem(R.id.list_toolbar_sort);
         MenuItem addItem = menu.findItem(R.id.list_toolbar_add);
 
@@ -162,11 +173,92 @@ public class SongListFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.list_toolbar_filter:
-                //TODO
+            case R.id.list_toolbar_filter_state_0:
+                mFilterStateIsActive = true;
+                mFilterState = R.id.list_toolbar_filter_state_0;
+                item.setChecked(true);
+                getActivity().invalidateOptionsMenu();
                 return true;
-            case R.id.list_toolbar_sort:
-                //TODO
+            case R.id.list_toolbar_filter_state_1:
+                mFilterStateIsActive = true;
+                mFilterState = R.id.list_toolbar_filter_state_1;
+                item.setChecked(true);
+                getActivity().invalidateOptionsMenu();
+                return true;
+            case R.id.list_toolbar_filter_state_2:
+                mFilterStateIsActive = true;
+                mFilterState = R.id.list_toolbar_filter_state_2;
+                item.setChecked(true);
+                getActivity().invalidateOptionsMenu();
+                return true;
+            case R.id.list_toolbar_filter_state_3:
+                mFilterStateIsActive = true;
+                mFilterState = R.id.list_toolbar_filter_state_3;
+                item.setChecked(true);
+                getActivity().invalidateOptionsMenu();
+                return true;
+            case R.id.list_toolbar_filter_difficulty_0:
+                mFilterDifficultyIsActive = true;
+                mFilterDifficulty = R.id.list_toolbar_filter_difficulty_0;
+                item.setChecked(true);
+                getActivity().invalidateOptionsMenu();
+                return true;
+            case R.id.list_toolbar_filter_difficulty_1:
+                mFilterDifficultyIsActive = true;
+                mFilterDifficulty = R.id.list_toolbar_filter_difficulty_1;
+                item.setChecked(true);
+                getActivity().invalidateOptionsMenu();
+                return true;
+            case R.id.list_toolbar_filter_difficulty_2:
+                mFilterDifficultyIsActive = true;
+                mFilterDifficulty = R.id.list_toolbar_filter_difficulty_2;
+                item.setChecked(true);
+                getActivity().invalidateOptionsMenu();
+                return true;
+            case R.id.list_toolbar_filter_difficulty_3:
+                mFilterDifficultyIsActive = true;
+                mFilterDifficulty = R.id.list_toolbar_filter_difficulty_3;
+                item.setChecked(true);
+                getActivity().invalidateOptionsMenu();
+                return true;
+            case R.id.list_toolbar_filter_difficulty_4:
+                mFilterDifficultyIsActive = true;
+                mFilterDifficulty = R.id.list_toolbar_filter_difficulty_4;
+                item.setChecked(true);
+                getActivity().invalidateOptionsMenu();
+                return true;
+            case R.id.list_toolbar_filter_default:
+                mFilterStateIsActive = false;
+                mFilterDifficultyIsActive = false;
+                getActivity().invalidateOptionsMenu();
+                return true;
+            case R.id.list_toolbar_sort_lastplayed:
+                mSortIsActive = true;
+                mSort = R.id.list_toolbar_sort_lastplayed;
+                item.setChecked(true);
+                getActivity().invalidateOptionsMenu();
+                return true;
+            case R.id.list_toolbar_sort_score:
+                mSortIsActive = true;
+                mSort = R.id.list_toolbar_sort_score;
+                item.setChecked(true);
+                getActivity().invalidateOptionsMenu();
+                return true;
+            case R.id.list_toolbar_sort_totaltime:
+                mSortIsActive = true;
+                mSort = R.id.list_toolbar_sort_totaltime;
+                item.setChecked(true);
+                getActivity().invalidateOptionsMenu();
+                return true;
+            case R.id.list_toolbar_sort_totalcount:
+                mSortIsActive = true;
+                mSort = R.id.list_toolbar_sort_totalcount;
+                item.setChecked(true);
+                getActivity().invalidateOptionsMenu();
+                return true;
+            case R.id.list_toolbar_sort_default:
+                mSortIsActive = false;
+                getActivity().invalidateOptionsMenu();
                 return true;
             case R.id.list_toolbar_add:
                 FragmentManager fm = getActivity().getSupportFragmentManager();
@@ -178,6 +270,47 @@ public class SongListFragment extends Fragment {
                 return super.onOptionsItemSelected(item);
         }
 
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        //Deal with changes related to filter options
+
+        MenuItem filterItem = menu.findItem(R.id.list_toolbar_filter);
+        Drawable filterIcon = filterItem.getIcon();
+        MenuItem filterStateItem = menu.findItem(R.id.list_toolbar_filter_state);
+        MenuItem filterDifficultyItem = menu.findItem(R.id.list_toolbar_filter_difficulty);
+
+        filterStateItem.setChecked(mFilterStateIsActive);
+        filterDifficultyItem.setChecked(mFilterDifficultyIsActive);
+        if (mFilterStateIsActive || mFilterDifficultyIsActive){
+            filterIcon.setTint(getResources().getColor(R.color.difficulty_very_hard, getContext().getTheme()));
+            if (mFilterStateIsActive){
+                MenuItem activeStateFilterItem = menu.findItem(mFilterState);
+                activeStateFilterItem.setChecked(true);
+            }
+            if (mFilterDifficultyIsActive) {
+                MenuItem activeDifficultyFilterItem = menu.findItem(mFilterDifficulty);
+                activeDifficultyFilterItem.setChecked(true);
+            }
+        } else {
+            filterIcon.setTint(getResources().getColor(R.color.white, getContext().getTheme()));
+        }
+
+        //Deal with changes related to sort options
+
+        MenuItem sortItem = menu.findItem(R.id.list_toolbar_sort);
+        Drawable sortIcon = sortItem.getIcon();
+
+        if (mSortIsActive) {
+            sortIcon.setTint(getResources().getColor(R.color.difficulty_very_hard, getContext().getTheme()));
+            MenuItem activeSortItem = menu.findItem(mSort);
+            activeSortItem.setChecked(true);
+        } else {
+            sortIcon.setTint(getResources().getColor(R.color.white, getContext().getTheme()));
+        }
+
+        super.onPrepareOptionsMenu(menu);
     }
 
     @Override
